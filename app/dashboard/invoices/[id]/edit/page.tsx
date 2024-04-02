98156468b906196bbff2ad8c1f71747273e1d92b
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-02 15:50:55 -0600

    feat: manejo de errores

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import { notFound } from 'next/navigation'
 
 /**
  * Además de searchParams, los componentes de la página también aceptan un prop llamado params 
@@ -11,6 +12,8 @@ export default async function Page ({ params }: { params: { id: string } }) {
   const { id } = params
 
   const [invoce, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
+
+  if (!invoce) { notFound() }
 
   return (
     <main>

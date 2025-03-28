399970f4da1f23fdbc8cbf83e1122ff8e85f101b
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-03-27 19:40:49 -0600

    chore: delete files

--- schemas/generalInfo.tsx
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const generalInfoSchema = z.object({
-  title: z.string().min(2, {message: "El nombre debe tener al menos 2 caracteres"}),
-  description: z.string().min(10, {message: "La descripción debe tener al menos 10 caracteres"}),
-});
-
-export type GeneralInfo = z.infer<typeof generalInfoSchema>;

e4ce8638d9ae32dd6896ef1ae49a96d7c7557248
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-05 18:21:52 -0600

    feat: hook para controlar los inputs

--- hooks/useForm.ts
+++ hooks/useForm.ts
@@ -1,10 +1,5 @@
 import { useCallback, useState } from 'react';
 
-interface UseFormProps<T> {
-    form: T;
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  }
-
 export function useForm<T extends Record<string, any>>(initialState: T) {
   const [form, setForm] = useState<T>(initialState);
 
